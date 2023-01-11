cf9891ec6f4546178a2cbd698d1e5d08b279e34e
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2023-01-11 13:59:26 -0300

    Create middlewares:ensureAdmin , ensureAuth , dataIsValid

---
+++ src/middlewares/ensureAdminMiddleare.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import 'dotenv/config';
+import AppDataSource from "../data-source";
+import User from "../entities/user.entity";
+
+
+const  ensureAdminMiddleare = async(req: Request, res: Response, next: NextFunction) => {
+    const userRepository = AppDataSource.getRepository(User)
+
+    const userId = req.user.id
+    const user = await userRepository.findOneBy({ id: userId })
+
+    if(!user!.isAdm){
+        res.status(403).json({ message: "Access denied , only admin actored." })
+    }
+    
+    return next()
+
+}
+
+export default ensureAdminMiddleare;
