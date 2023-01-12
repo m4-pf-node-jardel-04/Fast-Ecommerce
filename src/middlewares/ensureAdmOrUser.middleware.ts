1d84ddb6b5d9eebc2979139212e8aa0dd229f2d6
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-01-12 11:50:55 -0300

    feat: middleware to verify is adm or user for list user address

---
+++ src/middlewares/ensureAdmOrUser.middleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../errors/AppError";
+
+const ensureAdmOrUserMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.user.isAdm && req.user.id !== req.params.id) {
+    throw new AppError("You don't have authorization!", 401);
+  }
+
+  return next();
+};
+
+export default ensureAdmOrUserMiddleware;
