739e01cf72bed416e1bdf04054b1181111e37ba4
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-01-13 14:07:13 -0300

    fix: list all address

--- src/services/addresses/listAddresses.service.ts
+++ src/services/addresses/listAddresses.service.ts
@@ -1,6 +1,5 @@
 import dataSource from "../../data-source";
 import Address from "../../entities/addresses.entity";
-import { returnedAddressDataSerializer } from "../../serializers/address.serializers";
 import { IAddress } from "../../interfaces/address.interfaces";
 
 const listAddressesService = async (): Promise<IAddress[]> => {
@@ -8,14 +7,7 @@ const listAddressesService = async (): Promise<IAddress[]> => {
 
   const addresses = await addressRepo.find();
 
-  const returnedData: IAddress[] = await returnedAddressDataSerializer.validate(
-    addresses,
-    {
-      stripUnknown: true,
-    }
-  );
-
-  return returnedData;
+  return addresses;
 };
 
 export default listAddressesService;
