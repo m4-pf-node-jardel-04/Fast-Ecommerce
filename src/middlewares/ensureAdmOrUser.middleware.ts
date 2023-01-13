5ad98d5491d20c7f5833374ee78c6ac22d5cc9d3
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-01-13 20:23:05 -0300

    fix: delete only own address

--- src/middlewares/ensureAdmOrUser.middleware.ts
+++ src/middlewares/ensureAdmOrUser.middleware.ts
@@ -1,12 +1,20 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "../errors/AppError";
+import User from "../entities/user.entity";
+import "dotenv/config";
+import dataSource from "../data-source";
 
 const ensureAdmOrUserMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (!req.user.isAdm && req.user.id !== req.params.id) {
+  const userRepository = dataSource.getRepository(User);
+  const user = await userRepository.findOneBy({
+    id: req.user.id,
+  });
+  console.log(user.address.id);
+  if (!req.user.isAdm && user.address.id !== req.params.id) {
     throw new AppError("You don't have authorization!", 401);
   }
 
